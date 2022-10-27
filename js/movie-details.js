d536d60335c8545ea5eddae39077f3a5f65a1f94
alyoneek <alyonta03@mail.ru>
2022-10-27 14:09:59 +0700

    add average score nearby title

--- js/movie-details.js
+++ js/movie-details.js
@@ -12,12 +12,16 @@ function loadMovieInfo(id) {
     })
     .then((json) => {
         let movieInfoContainer = $("#movie-info");
+        let averageRating = json.reviews.length ? calculateAverageRating(json.reviews) : 0;
+
         movieInfoContainer.find(".movie-title").text(json.name);
+        movieInfoContainer.find(".movie-average-rating").text(averageRating === 0 ? "-" : averageRating);
+        movieInfoContainer.find(".movie-average-rating").addClass(averageRating <= 5 && averageRating > 0 ? "red" : "green");
         movieInfoContainer.find(".brief-description").text(json.description);
         movieInfoContainer.find(".movie-poster").attr("src", json.poster);
         movieInfoContainer.find(".movie-year").text(json.year);
         movieInfoContainer.find(".movie-country").text(json.country);
-        //movieInfoContainer.find(".movie-genres").text(json.description);
+        movieInfoContainer.find(".movie-genres").text(genrateStringGenres(json.genres));
         movieInfoContainer.find(".movie-time").text(json.time);
         movieInfoContainer.find(".movie-tagline").text(json.tagline);
         movieInfoContainer.find(".movie-director").text(json.director);
@@ -25,4 +29,16 @@ function loadMovieInfo(id) {
         movieInfoContainer.find(".movie-fees").text(json.fees);
         movieInfoContainer.find(".movie-age-limit").text(json.ageLimit);
     });
+}
+
+function calculateAverageRating(reviews) {
+    return reviews.length === 0 ? 0 : normalizeScore(reviews.reduce((result, reviewObject) => (result + reviewObject.rating), 0) / reviews.length);
+}
+
+function normalizeScore(score) {
+    return Math.round(score * 10) / 10;
+}
+
+function genrateStringGenres(genres) {
+    return genres.reduce((genresArray, genreObject) => [...genresArray, genreObject.name], []).join(", ");
 }
