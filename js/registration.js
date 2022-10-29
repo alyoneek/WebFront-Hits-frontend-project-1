58a7ad93659751261fb2d69133ca53c6263fa7e4
alyoneek <alyonta03@mail.ru>
2022-10-29 15:13:32 +0700

    add registration request
    user can registrate using form
    errors of registration are not shown

--- js/registration.js
+++ js/registration.js
@@ -1,50 +1,49 @@
 $(document).ready(function () {
-    $("#signup-form").validate({
-        // in 'rules' user have to specify all the constraints for respective fields
-        rules: {
-            login: "required",
-            // lastname: "required",
-            // username: {
-            //     required: true,
-            //     minlength: 2 //for length of lastname
-            // },
-            password: {
-                required: true,
-                minlength: 6
-            },
-            confirm_password: {
-                required: true,
-                minlength: 6,
-                equalTo: "#password" //for checking both passwords are same or not
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            name: "required",
-        },
-        // in 'messages' user have to specify message as per rules
-        messages: {
-            login: "Введите логин",
-            // lastname: " Please enter your lastname",
-            // username: {
-            //     required: " Please enter a username",
-            //     minlength: " Your username must consist of at least 2 characters"
-            // },
-            password: {
-                required: "Введите пароль",
-                minlength: "Минимальная длина пароля - 6 символов"
-            },
-            confirm_password: {
-                required: "Введите пароль",
-                minlength: "Минимальная длина пароля - 6 символов",
-                equalTo: "Пароль не совпадает"
-            },
-            email: {
-                required: "Введите email",
-                email: "Введите корректный email"
-            },
-            name: "Введите ФИО",
+    $("#signup-form").submit(function (event) {
+        event.preventDefault();
+
+        let objectData = getObjectData();
+        if (objectData) {
+            fetch("https://react-midterm.kreosoft.space/api/account/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(objectData)
+            })
+            .then(response => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+            })
+            .then(data => console.log(data))
+            .catch(error => console.log(error));       
         }
     });
 });
+
+function getFormData() {
+    let inputs = $(".form-control");
+    let formData = new FormData();
+
+    for (let input of inputs) {
+        if ($(input).hasClass("error")) {
+            return null;
+        }
+        formData.append($(input).attr("id"), $(input).val());
+    }
+    formData.delete("confirm_password");
+    return formData;
+}
+
+function getObjectData() {
+    let formData = getFormData();
+    if (formData) {
+        let objectData = Object.fromEntries(formData);
+        objectData.gender = parseInt($("#gender").val());
+        console.log(objectData);
+
+        return objectData;
+    }
+    
+    return null;
+}
